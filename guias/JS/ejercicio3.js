3872b215637597a1035599a5beeee8b38de9974b
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-10-17 16:27:32 -0300

    Ejercicio 3 terminado y comentado

--- guias/JS/ejercicio3.js
+++ guias/JS/ejercicio3.js
@@ -4,9 +4,8 @@ class Trabajador {
         this.nombre = nombre;
         this.sueldo = sueldo;
     }
-
     mostrarDetalles() {
-        console.log(`Nombre: ${this.nombre}, Sueldo: ${this.sueldo}`);
+        console.log(`Nombre: ${this.nombre}, Sueldo: $${this.sueldo}`);
     }
 }
 
@@ -16,23 +15,34 @@ class Gerente extends Trabajador {
         super(nombre, sueldo);
         this.departamento = departamento;
     }
-
     // Sobrescribe el método para mostrar también el departamento
     mostrarDetalles() {
-        console.log(`Nombre: ${this.nombre}, Sueldo: ${this.sueldo}, Departamento: ${this.departamento}`);
+        console.log(`Nombre: ${this.nombre}, Sueldo: $${this.sueldo}, Departamento: ${this.departamento}`);
     }
 }
 
 // Crear instancias de Trabajador
-let trabajador1 = new Trabajador("Juan", 3000);
-let trabajador2 = new Trabajador("Pedro", 3500);
+let trabajador1 = new Trabajador("Ricardo", 530000);
+let trabajador2 = new Trabajador("Javier", 600000);
+let trabajador3 = new Trabajador("Carola", 564000);
+let trabajador4 = new Trabajador("Esteban", 500000);
 
 // Crear instancias de Gerente
-let gerente1 = new Gerente("Ana", 5000, "Recursos Humanos");
-let gerente2 = new Gerente("Luis", 6000, "IT");
+let gerente1 = new Gerente("Ana", 865000, "Recursos Humanos");
+let gerente2 = new Gerente("Luis", 916000, "IT");
+let gerente3 = new Gerente("Isabel", 863000, "Finanzas");
+let gerente4 = new Gerente("Sofía", 974000, "Administración")
 
-// Mostrar detalles de los empleados
-trabajador1.mostrarDetalles(); // Nombre: Juan, Sueldo: 3000
-trabajador2.mostrarDetalles(); // Nombre: Pedro, Sueldo: 3500
-gerente1.mostrarDetalles();    // Nombre: Ana, Sueldo: 5000, Departamento: Recursos Humanos
-gerente2.mostrarDetalles();    // Nombre: Luis, Sueldo: 6000, Departamento: IT
+// Mostrar información de la clase trabajador
+console.log("Trabajadores:");
+trabajador1.mostrarDetalles(); 
+trabajador2.mostrarDetalles();
+trabajador3.mostrarDetalles();
+trabajador4.mostrarDetalles();
+
+// Mostrar información de la clase gerente
+console.log("Gerencia:");
+gerente1.mostrarDetalles();    
+gerente2.mostrarDetalles();
+gerente3.mostrarDetalles();
+gerente4.mostrarDetalles();
