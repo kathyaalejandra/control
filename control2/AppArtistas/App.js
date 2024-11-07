fb2f3fca25df1a0d5330e8ae4bdcabf76b2404d4
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-11-06 21:04:23 -0300

    App Lista

--- control2/AppArtistas/App.js
+++ control2/AppArtistas/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, Pressable, View, Alert } from 'react-native';
+import { StyleSheet, Text, Pressable, View, Image, Alert } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons, FontAwesome } from '@expo/vector-icons'; 
@@ -7,7 +7,7 @@ import HomeScreen from './HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-{/* Marcelo Bustamante - Kathya Mansilla*/}
+// Marcelo Bustamante - Kathya Mansilla
 
 function MainScreen() {
   const navigation = useNavigation();
@@ -18,12 +18,76 @@ function MainScreen() {
         style={styles.iconButton} 
         onPress={() => navigation.navigate('Home')}
       >
+        <Image 
+          source={require('./assets/camiloo.png')}
+          style={styles.imagenprincipal}
+        />
       </Pressable>
+      <Text style={styles.text}>Camilo</Text>
+      <Text style={styles.subtitulo}>20 Canciones</Text>
+
+      <View style={styles.rowContainer}>
+        <View style={styles.artistContainer}>
+          <Pressable 
+            style={styles.iconButton} 
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Image 
+              source={require('./assets/bunkers.jpg')}
+              style={styles.icon}
+            />
+          </Pressable>
+          <Text style={styles.text}>Los Bunkers</Text>
+          <Text style={styles.subtitulo}>8 Canciones</Text>
+        </View>
+        <View style={styles.artistContainer}>
+          <Pressable 
+            style={styles.iconButton} 
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Image 
+              source={require('./assets/jere.jpg')}
+              style={styles.icon}
+            />
+          </Pressable>
+          <Text style={styles.text}>Jere Klein</Text>
+          <Text style={styles.subtitulo}>10 Canciones</Text>
+        </View>
+      </View>
+
+      <View style={styles.rowContainer}>
+        <View style={styles.artistContainer}>
+          <Pressable 
+            style={styles.iconButton} 
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Image 
+              source={require('./assets/marciano.jpg')}
+              style={styles.icon}
+            />
+          </Pressable>
+          <Text style={styles.text}>Marcianeke</Text>
+          <Text style={styles.subtitulo}>9 Canciones</Text>
+        </View>
+        <View style={styles.artistContainer}>
+          <Pressable 
+            style={styles.iconButton} 
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Image 
+              source={require('./assets/miranda.jpg')}
+              style={styles.icon}
+            />
+          </Pressable>
+          <Text style={styles.text}>Miranda</Text>
+          <Text style={styles.subtitulo}>17 Canciones</Text>
+        </View>
+      </View>
 
       <View style={styles.buttonContainer}>
         <Pressable 
           style={styles.button} 
-          onPress={() => Alert.alert("Alerta", "Has presionado el botón normal")}
+          onPress={() => Alert.alert("Favoritos", "¿Deseas agregar alguno de estos artistas a tu playlist?")}
         >
           <Ionicons name="musical-note" size={20} color="white" style={styles.iconLeft} />
           <Text style={styles.buttonText}>Agregar a la playlist</Text>
@@ -58,6 +122,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  artistContainer: {
+    alignItems: 'center',
+    width: '45%',
+  },
+  iconButton: {
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 8,
+  },
+  icon: {
+    width: 120,
+    height: 120,
+    borderRadius: 5,
+  },
+  imagenprincipal: {
+    width: 350,
+    height: 220,
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: 'black',
+    marginTop: 5,
+  },
+  subtitulo: {
+    fontSize: 14,
+    color: 'black',
   },
   buttonContainer: {
     flexDirection: 'row',
--- control2/AppArtistas/HomeScreen.js
+++ control2/AppArtistas/HomeScreen.js
@@ -5,8 +5,8 @@ const Artista = ({ artist, onClose }) => {
   return (
     <View style={styles.infoContainer}>
       <Text style={styles.artistName}>CAMILO </Text>
+      <Text style={styles.genero}>Pop</Text>
       <Text style={styles.artistDescription}> Camilo Echeverry Correa, conocido en el medio artístico como Camilo, es un cantante, actor y compositor colombiano. Nació el 16 de marzo de 1994 en Medellín, Colombia. En 2007 se dio a conocer al ganar en el programa de talentos "Factor X", transmitido por la televisión de su país.</Text>
-      <Button title="Cerrar" onPress={onClose} color="#FF5733" />
     </View>
   );
 };
@@ -22,11 +22,15 @@ const styles = StyleSheet.create({
   artistName: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 5,
+  },
+  genero: {
+  fontSize: 16,
+  marginBottom: 5,
   },
   artistDescription: {
     textAlign: 'justify',
-    marginBottom: 20,
+    marginBottom: 5,
   },
 });
 
